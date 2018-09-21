527eb6809ee0655ecea80060a16885e5ec1589b1
ashleyxwang <ashleywang514@yahoo.com>
2018-09-20 19:20:46 -0500

    initialize html and js pages

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+//pseudocode functions
+//crystals into buttons
+
+
+//declare variables
+var userSum = 0;
+var targetNumber = 0;
+var crystalA = 0;
+var crystalB = 0;
+var crystalC = 0;
+var crystalD = 0;
+var totalWins = 0;
+var totalLosses = 0;
+var winMessage = "";
+var loseMessage = "";
+
+
+//total wins, total loses
+
+//reset function
+    //display user score = 0
+    //pick random number between 19-120 and display it 
+    //assign random number to each crystal between 1-12 (do not display number)
+
+//make crystals clickable
+//player click crystal, add number to user score
+//if score is above random number, loss++
+//if score === random number, win++
+
+//update total wins and total loses, reset
